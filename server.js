fc0c11566cdcdc7f06b6a34b195699274a181f20
Harry Davis <harrytobydavis@gmail.com>
2020-07-31 22:00:11 +0100

    Updated for heroku

--- server.js
+++ server.js
@@ -27,11 +27,12 @@ app.use(
   })
 );
 
-app.use(express.static('public'));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 //SETTING UP MONGOOSE
 console.log('Connecting to MongoDB');
